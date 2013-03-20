2a3bb2f66dbd2ca8c99cabf4a28664776304d4e7
delejnr <bj-r1@live.com>
2013-03-20 05:33:48 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photorails::Application.config.secret_token = 'f0d13c6919bdb06aedfc331d2580d553377452b0dd687db12e0ebf580748cdbb187f6f76a81826d7b4c5108f1fc5cf445971959ab5b1a8a49a0600c2889c65e2'
